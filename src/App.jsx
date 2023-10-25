a390fa10154dfae19bfa3024389919c619111105
unknown <marshallanique@gmail.com>
2023-10-25 18:00:04 +0600

    user created

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-
-import { useState } from 'react';
-import './App.css'
-import { useEffect } from 'react';
+import { useState } from "react";
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
   const [products, setProducts] = useState([]);
 
   const fetchData = async () => {
     try {
-      const response = await fetch(
-        "http://localhost:5000/users"
-      );
+      const response = await fetch("http://localhost:5000/users");
       const data = await response.json();
       setProducts(data);
     } catch (error) {
@@ -22,12 +19,39 @@ function App() {
     fetchData();
   }, []);
 
+  const handleSub = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts([...products, data]);
+      });
+  };
 
   return (
-  <div>
-   {products.length}
-  </div>
-  )
+    <div className="text-3xl flex justify-center">
+      <form onSubmit={handleSub}>
+        <div className="flex flex-col w-[100%] gap-y-5 pt-10">
+          <input className="border" type="text" name="name" id="" />
+          <input className="border" type="email" name="email" id="" />
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+      {products.map((item) => {
+        return <div key={item.id}>{item.name}</div>;
+      })}
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      Home
+      <h3 className="text">hh</h3>
+      <p>h</p>
+      <h3>33</h3>
+    </div>
+  );
+};
+
+export default Home;
