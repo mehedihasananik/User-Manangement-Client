857b7e9cd8e378ecd088d9a15620c05db98e246f
unknown <marshallanique@gmail.com>
2023-10-26 14:46:57 +0600

    form created

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,40 @@ const App = () => {
   useEffect(() => {
     fetchData();
   }, []);
-  console.log(users);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <div>
+      <div className="flex justify-center pt-5">
+        <form className="flex flex-col w-[25%] gap-y-2" onSubmit={handleSubmit}>
+          <input
+            className="border px-2 py-1 capitalize "
+            type="text"
+            name=""
+            id=""
+            placeholder="name"
+          />
+          <input
+            className="border px-2 py-1 capitalize "
+            type="email"
+            name=""
+            id=""
+            placeholder="email"
+          />
+          <div className="flex justify-center">
+            <button
+              className="text-white bg-blue-600 py-1.5 w-28 rounded-md"
+              type="submit"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+
       <div className="pt-5">
         {users.map((user) => {
           return (
